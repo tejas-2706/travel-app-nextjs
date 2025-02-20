fbc7745bbbdb523ad2314003cfc80a1452f9ef86
tejas <pangaonkar.svc424@gmail.com>
2025-02-20 22:50:23 +0530

    added mobile changes/HamburgerMenu

--- components/AppbarSection.tsx
+++ components/AppbarSection.tsx
@@ -1,26 +1,47 @@
-"use client"
-import { ModeToggle } from "./my_ui/ModeToggle";
-import { UserAuth } from "./UserAuth"
-import logo from "../public/logo.png"
+"use client";
 
+import { ModeToggle } from "./my_ui/ModeToggle";
+import { UserAuth } from "./UserAuth";
+import logo from "../public/logo.png";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import HamburgerMenu from "./HamburgerMenu";
 export const Appbar = () => {
-    return (
-        <div className="flex justify-around border p-4 shadow-md">
-            <ul className="flex gap-2">
-                <div className="relative ">
-                <Link href={'/dashboard'}><Image className="" src={logo} alt="logo" width={30} /></Link>
-                </div>
-                {/* <div className="text-2xl font-bold px-4 rounded-lg">Travel App </div> */}
-                <Link href={'/book-now'} ><Button variant={"link"}>Book Now</Button></Link>
-                <Link href={'/about'} ><Button variant={"link"}>About</Button></Link>
-            </ul>
-            <div className="flex gap-4">
-                <UserAuth />
-                <ModeToggle />
-            </div>
+  return (
+    <div className="flex items-center border p-4 shadow-md w-full">
+      {/* Left Section: Hamburger Menu (mobile), Logo, and Navigation Links */}
+      <div className="flex items-center">
+        {/* Hamburger Menu (visible only on mobile) */}
+        <div className="md:hidden">
+          <HamburgerMenu/>
         </div>
-    )
-}
+        {/* Logo (always visible) */}
+        <div className="ml-2">
+          <Link href="/dashboard">
+            <Image src={logo} alt="logo" width={30} />
+          </Link>
+        </div>
+        {/* Navigation Links (visible only on web, close to logo) */}
+        <ul className="hidden md:flex gap-4 ml-4">
+          <li>
+            <Link href="/book-now">
+              <Button variant="link">Book Now</Button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <Button variant="link">About</Button>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Right Section: UserAuth and ModeToggle (visible only on web, pushed to right) */}
+      <div className="flex md:pr-24 gap-4 ml-auto">
+        <UserAuth />
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
